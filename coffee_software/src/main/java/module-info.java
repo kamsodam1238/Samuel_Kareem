1e13ed746864aa3a512cf907b61af1e42ee2f585
kamsodam1238 <kamsodam1238@gmail.com>
2023-04-26 20:23:59 -0500

    new

--- coffee_software/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module coffee.coffee_software {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens coffee.coffee_software to javafx.fxml;
-    exports coffee.coffee_software;
-}
